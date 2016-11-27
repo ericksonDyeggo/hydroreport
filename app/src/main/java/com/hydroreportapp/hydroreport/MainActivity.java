2dec7530c84dd2ff07aa17a056018160d561895c
Matheus Connolyn <Connolyn@MacBook-Air-de-Matheus.local>
2016-11-27 00:08:32 -0200

    tela logo

--- app/src/main/java/com/hydroreportapp/hydroreport/MainActivity.java
+++ app/src/main/java/com/hydroreportapp/hydroreport/MainActivity.java
@@ -1,18 +1,31 @@
 package com.hydroreportapp.hydroreport;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
-//                .requestEmail()
-//                .build();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_report);
+                /* Create an Intent that will start the Menu-Activity. */
+                //Intent mainIntent = new Intent(MainActivity.this,ActivityReport.class);
+                //MainActivity.this.startActivity(mainIntent);
+               // MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
+
 }
