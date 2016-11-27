e91b3530d5081ac4d223e2e8956ec071efd5b1e3
Erickson <ericksondyeggo@gmail.com>
2016-11-27 07:38:56 -0200

    onBackPressed OK

--- app/src/main/java/com/hydroreportapp/hydroreport/MapaActivity.java
+++ app/src/main/java/com/hydroreportapp/hydroreport/MapaActivity.java
@@ -157,4 +157,11 @@ public class MapaActivity extends FragmentActivity implements OnMapReadyCallback
         latLng = new LatLng(cord_x, cord_y);
         updatePosition();
     }
+
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
 }
