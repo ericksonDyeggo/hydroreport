cb1de7f8023abab6d078b1356504730cb839857c
Erickson <ericksondyeggo@gmail.com>
2016-11-26 23:17:19 -0200

    manifest com as autorizações

--- app/src/main/java/com/hydroreportapp/hydroreport/MainActivity.java
+++ app/src/main/java/com/hydroreportapp/hydroreport/MainActivity.java
@@ -9,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
-//                .requestEmail()
-//                .build();
     }
 }
